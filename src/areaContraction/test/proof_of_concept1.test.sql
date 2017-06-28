ce1d48497d8a0241d4d47d77a2567a99af89e6bf
Ankur Shukla <work.ankurshukla@gmail.com>
2017-06-29 01:27:23 +0530

    Modifying proof of concept

--- src/areaContraction/test/proof_of_concept1.test.sql
+++ src/areaContraction/test/proof_of_concept1.test.sql
@@ -14,5 +14,5 @@ a AS (SELECT * from pgr_dijkstra(
           'SELECT * FROM edge_table', ARRAY[1,4,7,13],  ARRAY[1,4,7,13]) where edge<>-1),
 b AS (SELECT edge as id,node as source,cost from a group by id,source,cost),
 c AS (SELECT id, source, target, cost, reverse_cost FROM edge_table),
-d AS (SELECT b.id,b.source,target,c.cost,reverse_cost from b join c on b.id=c.id)
+d AS (SELECT b.id,b.source,target,c.cost from b join c on b.id=c.id)
 select * from d;
